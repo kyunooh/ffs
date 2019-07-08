115f2ed278b778e7b3f1c06fd176fcece3740d7e
jelly <chm073@gmail.com>
2019-07-08 19:12:18 +0900

    Init project

---
+++ ffs/ffs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ffs
+//
+//  Created by jelly on 08/07/2019.
+//  Copyright © 2019 jellyms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
